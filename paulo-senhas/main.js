9fc612305f6ec0ea6e15fbd51a07a3a797d6fcaa
paulocardoso12 <159543601+paulocardoso12@users.noreply.github.com>
2024-08-06 14:58:05 -0300

    Add files via upload

--- paulo-senhas/main.js
+++ paulo-senhas/main.js
@@ -57,23 +57,19 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha(alfabeto.length);
+    classificaSenha();
 
 }
 
-function classificaSenha(tamanhoAlfabeto){
-    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-    console.log(entropia);
+function classificaSenha(){
     forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 57){
+    if (tamanhoSenha > 11){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 57 ) {
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
         forcaSenha.classList.add('media');
-    } else if (entropia <= 35){
+    } else if (tamanhoSenha <= 5){
         forcaSenha.classList.add('fraca');
     }
-    const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
 
 
